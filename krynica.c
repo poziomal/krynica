d1e8fcefc22ac452c2a6833ab988d46e55cc81cb
Poziomek77 <jakub.kuroczycki@gmail.com>
2023-02-14 21:15:46 +0100

    Add my first file reading code

--- krynica.c
+++ krynica.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
 int main(){
-    
+    FILE *fptr;
+    char *content =  (char*) malloc(10000000);
+    fptr=fopen("input.txt","r");
+    fscanf(fptr, "%s", content);
+    fclose(fptr);
+    printf("%s\n" , content);
+    free(content);
+       return 0;
 }
