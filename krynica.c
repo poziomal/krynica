5a24ad3ff35db6a7ae3642d2abef7b30c33c35ca
Poziomek77 <jakub.kuroczycki@gmail.com>
2023-02-17 20:33:49 +0100

    Add sequential file reading

--- krynica.c
+++ krynica.c
@@ -2,23 +2,31 @@
 #include <stdlib.h>
 int main(){
     FILE *fptr;
-    char *content =  (char*) malloc(10000000);
+    char *content =  (char*) malloc(999999999);
     fptr=fopen("input.txt","r");
-    fscanf(fptr, "%s", content);
+    int readBytes = 0;
+    int szczepIQ = 0;
+    do
+    {
+     szczepIQ = fscanf(fptr, "%c", content + readBytes);
+     readBytes += szczepIQ;
+    } while (szczepIQ>0);
+    content[readBytes+1] = 0;
     fclose(fptr);
     printf("%s\n" , content);
+    printf("%d\n" , szczepIQ);
     free(content);
     int size = sizeof(FILE);
     printf("Size of the type FILE is:%d\n",size);
-    int size2 = sizeof(*fptr);
+    int size2 = sizeof(FILE*);
     printf("Size of the pointer *fptr:%d\n",size2);
     int size3 = sizeof(int);
-     printf("Size of the type int is:%d\n",size3);
-      int size4 = sizeof(long);
-       printf("Size of the type long is:%d\n",size4);
-       int size5 = sizeof(int*);
-         printf("Size of the pointer *int:%d\n",size5);
-         int size6 = sizeof(long*);
-         printf("Size of the pointer *long:%d\n",size6);
-     return 0;
+    printf("Size of the type int is:%d\n",size3);
+    int size4 = sizeof(long);
+    printf("Size of the type long is:%d\n",size4);
+    int size5 = sizeof(int*);
+    printf("Size of the pointer *int:%d\n",size5);
+    int size6 = sizeof(long*);
+    printf("Size of the pointer *long:%d\n",size6);
+    return 0;
 }
