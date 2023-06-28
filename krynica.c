2e5afdf49a040015b3c9387c2dae5a8a557f3e90
Poziomek77 <jakub.kuroczycki@gmail.com>
2023-06-28 20:25:31 +0200

    ubdate

--- krynica.c
+++ krynica.c
@@ -27,17 +27,5 @@ int main(){
     fclose(fptr);
     printf("ilosc znakuw:%d\n" , readBytes);
     free(content);
-    int size = sizeof(FILE);
-    printf("Size of the type FILE is:%d\n",size);
-    int size2 = sizeof(FILE*);
-    printf("Size of the pointer *fptr:%d\n",size2);
-    int size3 = sizeof(int);
-    printf("Size of the type int is:%d\n",size3);
-    int size4 = sizeof(long);
-    printf("Size of the type long is:%d\n",size4);
-    int size5 = sizeof(int*);
-    printf("Size of the pointer *int:%d\n",size5);
-    int size6 = sizeof(long*);
-    printf("Size of the pointer *long:%d\n",size6);
     return 0;
 }
